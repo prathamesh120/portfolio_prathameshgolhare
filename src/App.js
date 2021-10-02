c04942bc0054a5ec558b9b237c130d6c5363fc38
Your Name <you@example.com>
2021-10-02 10:42:53 -0700

    2-10-21

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
-
+import Navbar from "./Components/Navbar";
 function App() {
   return (
-    <h1>Portfolio Project</h1>
+    <Navbar/>
   );
 }
 
---
+++ src/Components/Navbar.js
@@ -0,0 +1,43 @@
+import React from "react";
+import download from "../download.png"
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+    <div className="container">
+
+     <a className="navbar-brand" href="#"><img className="logo" src={download} alt ="Logo .."></img></a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+      
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Home <span class="sr-only"></span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">About me</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Services</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">How work</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Portfolio</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Contacts</a>
+            </li>
+         
+          </ul>
+         
+        </div>
+    </div>
+      </nav>
+    )
+}
+
+export default Navbar
